0b367598c3126094ec9c9eb5b056825941a9ae24
10igeorge <10izzygeorge@gmail.com>
2016-02-18 20:12:19 -0800

    create getter for user word

--- src/test/java/WordTest.java
+++ src/test/java/WordTest.java
@@ -4,8 +4,14 @@ import static org.junit.Assert.*;
 public class WordTest {
 
   @Test
-  public void Word_instantiatesCorrectly_true(){
+  public void word_instantiatesCorrectly_true(){
     Word newWord = new Word("Internet");
     assertEquals(true, newWord instanceof Word);
+  }
+
+  @Test
+  public void getWord_returnsWordCorrectly(){
+    Word newWord = new Word("Internet");
+    assertEquals("Internet", newWord.getWord());
   }
 }
