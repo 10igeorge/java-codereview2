6dd4f3391555e672adab0dd17ce9441c3a7caec4
10igeorge <10izzygeorge@gmail.com>
2016-02-18 21:36:29 -0800

    create definition classes and pass test that will clear definitions

---
+++ src/main/java/Definition.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Definition{
+  private static ArrayList<Definition> definitions = new ArrayList<Definition>();
+  private String mDefinition;
+  private int mId;
+
+
+  public static void clear(){
+    definitions.clear();
+  }
+  
+}
---
+++ src/test/java/DefinitionTest.java
@@ -0,0 +1,11 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class DefinitionTest{
+
+  @Rule
+  public ClearRule clearRule = new ClearRule();
+
+  // @Test
+  // public void definition_firsttest
+}
