ac7c094fbd9af50ee14988d2fdc8390f82cb58c7
10igeorge <10izzygeorge@gmail.com>
2016-02-19 16:40:52 -0800

    display everything properly one webpage aside from definitions

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,59 +12,61 @@ public class App {
 
     get("/", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
-      model.put("words", Word.all());
-
       model.put("template", "templates/index.vtl");
-      return new ModelAndView (model, layout);
+      return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    post("/update", (request, response) -> {
+    get("/words", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("words", Word.all());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/words", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
       String word = request.queryParams("word");
       Word newWord = new Word(word);
       model.put("words", Word.all());
-      model.put("words", Word.all());
-      model.put("template", "templates/success.vtl");
-      return new ModelAndView (model, layout);
-    }, new VelocityTemplateEngine());
-
-    get("/:id", (request, response) -> {
-      HashMap<String, Object> model = new HashMap<String, Object>();
-      Word word = Word.find(Integer.parseInt(request.params(":id")));
-      model.put("word", word);
-      model.put("template", "templates/word-info.vtl");
+      model.put("word", newWord);
+      model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    get("/:id/new-def", (request, response) -> {
+    get("/words/:id", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
       Word word = Word.find(Integer.parseInt(request.params(":id")));
       ArrayList<Definition> definitions = word.getAllDefinitions();
       model.put("word", word);
       model.put("definitions", definitions);
+      model.put("template", "templates/word-info.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+  get("/definitions/:id/new-def", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Word word = Word.find(Integer.parseInt(request.params(":id")));
+      model.put("word", word);
+      model.put("definitions", Definition.all());
       model.put("template", "templates/new-def.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    post("/updated", (request, response) -> {
-      HashMap<String, Object> model = new HashMap<String, Object>();
-      Word word = Word.find(Integer.parseInt(request.queryParams("wordId")));
-      ArrayList<Definition> definitions = request.session().attribute("definitions");
-//Create if null
-      if (definitions == null){
-        definitions = new ArrayList<Definition>();
-        request.session().attribute("definitions", definitions);
-      }
-//Put into array
-      String definition = request.queryParams("definition");
-      Definition newDef = new Definition(definition);
-      definitions.add(newDef);
-//Put on page
-      model.put("word", word);
-      model.put("definitions", definitions);
-      model.put("template", "templates/success.vtl");
-      return new ModelAndView (model, layout);
+  post("/definitions", (request, response) -> {
+    HashMap<String, Object> model = new HashMap<String, Object>();
+    Word word = Word.find(Integer.parseInt(request.queryParams("wordId")));
+    ArrayList<Definition> definitions = word.getAllDefinitions();
+    if (definitions == null) {
+      definitions = new ArrayList<Definition>();
+      request.session().attribute("definitions", definitions);
+    }
+    String userDef = request.queryParams("userDef");
+    Definition newDefinition = new Definition(userDef);
+    definitions.add(newDefinition);
+    model.put("definitions", definitions);
+    model.put("word", word);
+    model.put("template", "templates/word-info.vtl");
+    return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
-
   }
 }
